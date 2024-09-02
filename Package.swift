b7ddadafb8f5368f3314d6d7835262ddb4637b8d
Ethan Huang <eh13@hey.com>
2024-09-02 11:58:31 +0700

    Enable strict concurrency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NSAttributedStringBuilder",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [.enableUpcomingFeature("StrictConcurrency")]
         ),
         .testTarget(
             name: "NSAttributedStringBuilderTests",
